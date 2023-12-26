9944e4c5fde1b0f3bed646cf4f4dfaebba76dcb8
DoctorWho05 <niklas.buecker.nb@gmail.com>
2023-12-26 22:19:06 +0100

    Update Workspace.java

--- server/mimo/src/main/java/mimo/Controller/WorkspaceManager/Workspace.java
+++ server/mimo/src/main/java/mimo/Controller/WorkspaceManager/Workspace.java
@@ -1,13 +1,18 @@
 package mimo.Controller.WorkspaceManager;
 
+import mimo.Controller.PluginManager.PluginSet;
+
 public abstract class Workspace {
     private String mWorkspaceName;
+    protected PluginSet mPluginSet;
 
     public Workspace(String mWorkspaceName) {
         initWorksapce(mWorkspaceName);
     }
 
     private void initWorksapce(String mWorkspaceName) {
-
+        this.mWorkspaceName = mWorkspaceName;
     }
+
+    
 }
