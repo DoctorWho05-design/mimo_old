5d653b0b9eabf0663b4bc874fe147cc8ac0c431e
DoctorWho05 <niklas.buecker.nb@gmail.com>
2024-01-03 17:00:37 +0100

    create 2 New Plugins

---
+++ server/mimo/src/main/java/plugins/CodeManager/CodeManager.java
@@ -0,0 +1,31 @@
+package plugins.CodeManager;
+
+import mimo.Controller.Data;
+import mimo.Controller.Plugin.Plugin;
+
+public class CodeManager extends Plugin{
+
+    public CodeManager() {
+        super("CodeManager");
+        
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    @Override
+    public void run(Data mData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
---
+++ server/mimo/src/main/java/plugins/Minecraft/Minecraft.java
@@ -0,0 +1,31 @@
+package plugins.Minecraft;
+
+import mimo.Controller.Data;
+import mimo.Controller.Plugin.Plugin;
+
+public class Minecraft extends Plugin{
+
+    public Minecraft() {
+        super("Minecraft");
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void start() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+
+    @Override
+    public void run(Data mData) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'run'");
+    }
+    
+}
